03b225381ed3a980d6d24813a5ead1610e300dd4
Hariom <hariom4694@gmail.com>
2021-03-08 13:31:02 +0530

    Restructure and minor code change
    Signed-off-by: Hariom <hariom4694@gmail.com>

--- testing.py
+++ testing.py
@@ -8,25 +8,6 @@ from flask import request
 from flask import redirect,url_for
 from Linear_Model import model
 
-#def model(readings):
-#    import pandas as pd
-#    import numpy as np
-#
-#    data = pd.read_excel('ActData1.xlsx')
-#
-#    x= data[['Temp','Rh','Soil_Moist']] 
-#    y= data[['Water_content']]
-#    
-#    from sklearn.model_selection import train_test_split
-#    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3,random_state=101)
-#    
-#    from sklearn.linear_model import LinearRegression
-#    model = LinearRegression()
-#    model.fit(x_train,y_train)
-#    
-#    #predictions = model.predict(x_test)
-#    model.predict([readings])
-
 app = Flask(__name__)
 
 @app.route('/success')
